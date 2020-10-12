616231c30ed74e9f4f131ce12b85274fc056aa11
Sergio Urrutia-Oyer <urrutise@oregonstate.edu>
2020-10-11 20:47:49 -0700

    instantiated DynamicArray, capacity test, empty, len, append (to line 52)

--- dynamic_array.py
+++ dynamic_array.py
@@ -2,5 +2,32 @@
 # Your implementation should pass the tests in test_dynamic_array.py.
 # YOUR NAME
 
-class FixMe:
+import numpy as np
+
+class DynamicArray:
+
+    capacity = 10
+    arrLength = 0
+    arr = np.empty(0)
+
+    def __len__(self):
+        return self.arrLength
+
+    def __getitem__(self, item):
+        return self.arr[item]
+
+    def append(self, add):
+        if self.arrLength == 0:
+            self.arrLength += 1
+            self.arr =np.array([add])
+        else:
+            self.arrLength = (self.arrLength*2)
+            new_arr = np.arange(self.arrLength)
+            for x in new_arr:
+                self.arr[x] = x     # only works because of numpy defaults
+
+    def is_empty(self):
+        if self.arrLength == 0:
+            return True
+
     pass
